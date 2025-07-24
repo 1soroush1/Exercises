84c4a6d359f612a45290af04027a58093125972b
1soroush1 <165947280+1soroush1@users.noreply.github.com>
2025-07-24 14:34:22 +0330

    Add files via upload

---
+++ Stack-Queue/AdvanceQueue.java
@@ -0,0 +1,11 @@
+import java.util.PriorityQueue;
+
+public class AdvanceQueue<T> extends PriorityQueue<T> {
+    public T peekNext() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public void insertAtStart(T item) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
---
+++ Stack-Queue/AdvanceStack.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class AdvanceStack<T> extends Stack<T> {
+    public T peekSecondLast() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public void insertAtBottom(T item) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
